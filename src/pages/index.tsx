3a1f705ab92555beda9dd98451843f0854233f6b
Tyler Martin <tyler.martin@electric-playground.com>
2024-03-09 23:00:17 -0400

    adding basic layout .card styles, and link styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,30 @@
 import Link from 'next/link'
+import styles from '../styles/layout.module.css'
 
 export default function Home() {
   return (
     <div className="container">
-      <section>
-        <ul>
-          <li><Link href="/league/123">My Special NHL League</Link></li>
-          <li><Link href="league/456">MLB League</Link></li>
-        </ul>
-      </section>
-      <section>
-        <ul>
-          <li>invitation from Larry</li>
-          <li>invitation from Curly</li>
-          <li>invitation from Moe</li>
-        </ul>
-      </section>
+      <div className={styles.row}>
+        <section className={styles.col6}>
+          <div className="card">
+            <h3>Leagues:</h3>
+            <ul>
+              <li><Link className='link' href="/league/123">My Special NHL League <span>&rarr;</span></Link></li>
+              <li><Link className='link' href="league/456">MLB League <span>&rarr;</span></Link></li>
+            </ul>
+          </div>
+        </section>
+        <section className={styles.col6}>
+          <div className="card">
+            <h3>Invitations:</h3>
+            <ul>
+              <li>invitation from Larry</li>
+              <li>invitation from Curly</li>
+              <li>invitation from Moe</li>
+            </ul>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
