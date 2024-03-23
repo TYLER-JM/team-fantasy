ea7fdae408fedd27f67d3243ac865f94ac55eb83
Tyler Martin <tyler.martin@electric-playground.com>
2024-03-23 16:27:07 -0300

    adds CREATE league operation

--- src/pages/create-league/index.tsx
+++ src/pages/create-league/index.tsx
@@ -1,5 +1,71 @@
+import { useContext, useState } from "react"
+import { createClient } from "@/supbaseClient"
+import { AuthContext } from "@/context/AuthContext"
+
 export default function CreateLeague() {
+  const supabase = createClient()
+
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const auth = useContext(AuthContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    setLoading(true)
+
+    const formData = new FormData(event.target)
+    const leagueName = formData.get('name') as string
+    const realLifeLeague = formData.get('league') as string
+
+    let supabaseRes: {data: any, error: any}
+
+    supabaseRes = await supabase
+      .from('fantasy_leagues')
+      .insert([
+        {
+          commissioner_id: auth.user.id,
+          name: leagueName,
+          league_id: parseInt(realLifeLeague),
+          state_id: 1 // open
+        }
+      ])
+      .select()
+    
+      if (supabaseRes.error) {
+        setErrorMessage(supabaseRes.error.message)
+        setLoading(false)
+      } else {
+        console.log('We created a new league, yay!', supabaseRes.data)
+        setLoading(false)
+      }
+  }
+
   return (
-    <div className="container">Follow these steps to create a league</div>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-header">
+          <h3>Create a new league!</h3>
+        </div>
+        <div className="form-wrapper">
+
+          <span className="form-error text-s">{errorMessage}</span>
+          <div className="form-group">
+            <label htmlFor="name" className="form-label">Give your league a name:</label>
+            <input name="name" type="text" className="form-input" placeholder="The Champions League"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="league" className="form-label">Pick a real life league:</label>
+            <select name="league" className="form-input">
+              <option value="1">NHL</option>
+              <option value="2">NBA</option>
+            </select>
+          </div>
+          <div className="form-footer">
+            <button className="btn floating primary" disabled={loading}>{loading ? 'Saving...': 'Save & Continue'}</button>
+          </div>
+
+        </div>
+      </form>
+    </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,43 @@
 import Link from 'next/link'
 import styles from '../styles/layout.module.css'
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
 
 export default function Home() {
+  const auth = useContext(AuthContext)
+
   return (
     <div className="container">
-      <div className={styles.row}>
-        <section className={styles.col6}>
-          <div className="card">
-            <h3>Leagues:</h3>
-            <ul>
-              <li><Link className='link' href="/league/123">My Special NHL League <span>&rarr;</span></Link></li>
-              <li><Link className='link' href="league/456">MLB League <span>&rarr;</span></Link></li>
-            </ul>
-          </div>
-        </section>
-        <section className={styles.col6}>
-          <div className="card">
-            <h3>Invitations:</h3>
-            <ul>
-              <li>invitation from Larry</li>
-              <li>invitation from Curly</li>
-              <li>invitation from Moe</li>
-            </ul>
-          </div>
-        </section>
-      </div>
+      {auth.user ?
+        <div className={styles.row}>
+          <section className={styles.col6}>
+            <div className="card">
+              <h3>Leagues:</h3>
+              <ul>
+                <li><Link className='link' href="/league/123">My Special NHL League <span>&rarr;</span></Link></li>
+                <li><Link className='link' href="league/456">MLB League <span>&rarr;</span></Link></li>
+              </ul>
+            </div>
+          </section>
+          <section className={styles.col6}>
+            <div className="card">
+              <h3>Invitations:</h3>
+              <ul>
+                <li>invitation from Larry</li>
+                <li>invitation from Curly</li>
+                <li>invitation from Moe</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        :
+        <div className={styles.row}>
+          <section className={styles.col6}>
+            Please login to continue
+          </section>
+        </div>
+      }
+      
     </div>
   );
 }
